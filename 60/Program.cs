6231e670b189f7a414f93ef2997b693464bb561e
Anna Nalimova <avnalimova@mail.ru>
2023-03-27 23:10:18 +0300

    60

--- 60/Program.cs
+++ 60/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Сформулируйте трехмерный массив из неповторяющихся двузначных чисел.
 //Напишите программу , которая будет построчно выводить массив, добавляя индексы каждого элемента.
-Console.WriteLine("Введите  размеры массива через пробел: ");
+Console.Write("Введите размеры массива через пробел: ");
 string[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-int[,,] massive = GetArray(new int[] { int.Parse(numbers[0]), int.Parse(numbers[1]), int.Parse(numbers[2]) }, 1, 7);
-PrintArray(massive);
-if (FindEl(mass, res)) continue;
+int[,,] arr = GetArray(new int[] { int.Parse(numbers[0]), int.Parse(numbers[1]), int.Parse(numbers[2]) }, 1, 50);
+PrintArray(arr);
+
+
 int[,,] GetArray(int[] length, int min, int max)
 {
-    int[,,] mass = new int[length[0], length[1], length[2]];
+    int[,,] mass = new int[length[0], length[1],length[2]];
     for (int i = 0; i < mass.GetLength(0); i++)
     {
         for (int j = 0; j < mass.GetLength(1); j++)
@@ -16,7 +17,7 @@ int[,,] GetArray(int[] length, int min, int max)
             while (k < mass.GetLength(2))
             {
                 int res = new Random().Next(min, max + 1);
-                if (FindEl(mass, res)) continue;
+                if (FindElement(mass, res)) continue;
                 mass[i, j, k] = res;
                 k++;
             }
@@ -26,30 +27,30 @@ int[,,] GetArray(int[] length, int min, int max)
 }
 
 
-bool FindEl(int[,,] arr, int box )
+bool FindElement(int[,,] array, int a)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < arr.GetLength(2); k++)
+            for (int k = 0; k < array.GetLength(2); k++)
             {
-                if (arr[i, j, k] == box) return true;
+                if (arr[i, j, k] == a) return true;
             }
         }
     }
     return false;
 }
 
-void PrintArray(int[,,] arr)
+void PrintArray(int[,,] array)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < arr.GetLength(2); k++)
+            for (int k = 0; k < array.GetLength(2); k++)
             {
-               Console.Write($"{arr[i, j, k]} ({i},{j},{k})");
+                Console.Write($"{array[i, j, k]} ({i},{j},{k})   ");
             }
             Console.WriteLine();
         }
